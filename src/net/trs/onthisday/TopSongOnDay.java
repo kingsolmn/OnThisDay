9825370fa3d9403c08f06051c9adf5d2f121fb33
Steve Palacios <steve.palacios78@gmail.com>
2014-05-11 20:24:16 -0500

    Added some more of the UI. Wired up the UI date picker.

--- src/net/trs/onthisday/TopSongOnDay.java
+++ src/net/trs/onthisday/TopSongOnDay.java
@@ -7,10 +7,14 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 public class TopSongOnDay extends AsyncTask<String, Void, String>{
 
+	private static final String TAG = "SongOfTheDayBGThread";
+	
 	public String doInBackground(String... params){
+		Log.v(TAG, "Preparing to send for the song of the day");
 		String url   = params[0]; //SoD Service URI
 		String month = params[1]; //Month
 		String day   = params[2]; //Date
@@ -19,17 +23,21 @@ public class TopSongOnDay extends AsyncTask<String, Void, String>{
 		
 		Document doc = null;
 		try {
+			Log.i(TAG, "Sending for the song of the day");
 			doc = Jsoup.connect(url)
 					.data("month", month)
 					.data("day", day)
 					.data("year", year)
 					.post();
 		} catch (IOException e) {
+			Log.e(TAG, "Error getting the song of the day: " + e.getMessage());
 			e.printStackTrace();
+			return song;
 		}
 		Element result = doc.getElementById("result");
 		
 		if (result != null) {
+			Log.i(TAG, "Got the song of the day");
 			song = result.text();
 		}
 		
@@ -37,6 +45,7 @@ public class TopSongOnDay extends AsyncTask<String, Void, String>{
 	}
 	
 	protected void onPostExecute(String result){
+		Log.i(TAG, "Done, sending info back to the Main Activity");
 		MainActivity.songOnDayRes(result);
 	}
 }
