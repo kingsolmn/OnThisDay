943e5ab3fcae7702c057667e7134b03927e3440e
Steve Palacios <steve@teamrampage.net>
2015-03-08 09:29:18 -0700

    Finishing up Install Tracking
    Finished configuring analytics install tracking.

--- app/src/main/java/net/trs/onthisday/MainActivity.java
+++ app/src/main/java/net/trs/onthisday/MainActivity.java
@@ -3,6 +3,8 @@ package net.trs.onthisday;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.analytics.tracking.android.EasyTracker;
+
 
 public class MainActivity extends FragmentActivity {
 
@@ -19,53 +21,14 @@ public class MainActivity extends FragmentActivity {
         }
     }
 
+    @Override
+    public void onStart(){
+        super.onStart();
+        EasyTracker.getInstance(this).activityStart(this);
+    }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
-
-//    /**
-//     * A placeholder fragment containing a simple view.
-//     */
-//    public static class PlaceholderFragment extends Fragment {
-//
-//        public PlaceholderFragment() {
-//        }
-//
-//        @Override
-//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                                 Bundle savedInstanceState) {
-//            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-//            return rootView;
-//        }
-//    }
-
-//    public void lookUpDate(View v){
-//        MainFragment.lookUpDate(v);
-//    }
-//
-//    public static void songOnDayRes(String songOnDay){
-//        android.util.Log.i(TAG, "songOnDayRes");
-//        prg.setVisibility(View.INVISIBLE);
-//        btn.setVisibility(View.VISIBLE);
-//        tv.setText(songOnDay);
-//    }
+    @Override
+    public void onStop(){
+        EasyTracker.getInstance(this).activityStop(this);
+    }
 }
